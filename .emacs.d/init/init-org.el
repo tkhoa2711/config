fa37a0bfbeb8300bae89ba16cbd3e069fda21650
Khoa Le <ltkhoa2711@gmail.com>
2015-04-23 12:12:12 +0800

    highlight special keywords

--- .emacs.d/init/init-org.el
+++ .emacs.d/init/init-org.el
@@ -9,6 +9,12 @@
 ;; taking notes easily with org-capture
 (global-set-key "\C-c c" 'org-capture)
 
+;; highlight these keywords in orgmode
+(add-hook 'org-mode-hook
+          (lambda ()
+            (font-lock-add-keywords nil
+                                    '(("\\<\\(FIXME\\|TODO\\|NOTE\\):" 1 font-lock-warning-face t)))))
+
 (provide 'init-org)
 
 ;;; init-org.el ends here
