3bb8c831565b99d55aee7b5eaaca56fbda9c685c
Le Tan Khoa <tkhoa2711@yahoo.com.vn>
2015-09-19 11:26:55 +0800

    customize python-mode

--- .emacs.d/init/init-mode.el
+++ .emacs.d/init/init-mode.el
@@ -37,11 +37,34 @@
 (autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.\\(md\\|markdown\\)$" . markdown-mode))
 
+;; python-mode ------------------------
+;(setq py-install-directory "~/.emacs.d/elpa/python-mode")
+;(add-to-list 'load-path py-install-directory)
+;(require 'python-mode)
+(autoload 'python-mode "python-mode" "Major mode for python files" t)
+(add-to-list 'auto-mode-alist '("\\.\\(py\\|ipython\\)$" . python-mode))
+
+; use IPython
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+; use the wx backend, for both mayavi and matplotlib
+(setq py-python-command-args
+  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq py-force-py-shell-name-p t)
+
+; switch to the interpreter after executing code
+(setq py-shell-switch-buffers-on-execute-p t)
+(setq py-switch-buffers-on-execute-p t)
+; don't split windows
+(setq py-split-windows-on-execute-p nil)
+; try to automagically figure out indentation
+(setq py-smart-indentation t)
+
 
 ;; ============================================================================
 ;; other modes
 
-;; auto0revert-mode -------------------
+;; auto-revert-mode -------------------
 ;; tell emacs to refresh all the buffers from disk automatically
 ;; if the buffer is modified, it won't be reverted
 (global-auto-revert-mode t)
