0c3af2eff42b594ac8fbb7a2bfcc204bdb6af04c
Khoa Le <tkhoa2711@yahoo.com.vn>
2015-11-16 10:13:31 +0800

    fix minor typo

--- .emacs.d/init/init-hook.el
+++ .emacs.d/init/init-hook.el
@@ -1,6 +1,6 @@
 ;;; init-hook.el
 
-;; make script executable if it is stared with #/ symbol
+;; make script executable if it is started with #/ symbol
 (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
 
 (provide 'init-hook)
