fb0c2fcd2d3a3d892ef338a260fa149848b2adbe
Khoa Le <tkhoa2711@yahoo.com.vn>
2015-11-16 10:10:27 +0800

    add init module for emacs faces

---
+++ .emacs.d/init/init-face.el
@@ -0,0 +1,24 @@
+;;; init-face.el
+
+;; regex pattern matching:
+;; \\( and \\)        : grouping
+;; \\sw               : a word character
+;; \\s_               : a symbol character
+;; \\(\\sw\\|\\s_\\)+ : a variable name
+;; [ \t\n]*           : whitespace including newlines
+
+
+(defface font-lock-func-face
+  '((nil (:foreground "#7F0055" :weight bold))
+    (t (:bold t :italic t)))
+  "Font Lock mode face used for function calls."
+  :group 'font-lock-highlighting-face)
+
+(font-lock-add-keywords
+ 'emacs-lisp-mode
+ '(("(\\s-*\\(\\_<\\(?:\\sw\\|\\s_\\)+\\)\\_>"
+    1 'font-lock-func-face)))
+
+(provide 'init-face)
+
+;;; init-face.el ends here
