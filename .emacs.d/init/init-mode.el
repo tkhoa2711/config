7792565261c1fbf2c9be9f5767cfd592ef97b18f
Le Tan Khoa <tkhoa2711@yahoo.com.vn>
2015-11-20 08:00:59 +0800

    eval-when-compile slime; disable ido-mode

--- .emacs.d/init/init-mode.el
+++ .emacs.d/init/init-mode.el
@@ -67,30 +67,31 @@
 ;(load (expand-file-name "~/quicklisp/slime-helper.el"))
 
 ;; setup loadpath for SLIME manually
-(require 'slime)
-(add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
-(add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+(eval-when-compile
+  (require 'slime)
+  (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
+  (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
 
-;; specify the lisp program you are using. Default is "lisp"
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
+  ;; specify the lisp program you are using. Default is "lisp"
+  (setq inferior-lisp-program "/usr/local/bin/sbcl")
 
-;; custom core file to improve loading SLIME speed
-;; preload packages for sockets, posix, introspect, asdf
+  ;; custom core file to improve loading SLIME speed
+  ;; preload packages for sockets, posix, introspect, asdf
 
-;; those that take the most time to load.
-;;
-;; create the core by executing the following:
-;; * (mapc 'require '(sb-bsd-sockets sb-posix sb-introspect sb-cltl2 asdf))
-;; * (save-lisp-and-die "sbcl.core-for-slime")
-; (setq slime-lisp-implementations
-;       '((sbcl ("sbcl" "--core" "sbcl.core-for-slime"))))
+  ;; those that take the most time to load.
+  ;;
+  ;; create the core by executing the following:
+  ;; * (mapc 'require '(sb-bsd-sockets sb-posix sb-introspect sb-cltl2 asdf))
+  ;; * (save-lisp-and-die "sbcl.core-for-slime")
+  ;; (setq slime-lisp-implementations
+  ;;       '((sbcl ("sbcl" "--core" "sbcl.core-for-slime"))))
 
-; (setq slime-net-coding-system 'utf-8-unix)
+  ; (setq slime-net-coding-system 'utf-8-unix)
 
-;; set to load contrib packages
-(setq slime-contribs '(slime-fancy))                            ; almost everything (fancy)
-(slime-setup '(slime-fancy slime-asdf))
-
+  ;; set to load contrib packages
+  (setq slime-contribs '(slime-fancy))
+  (slime-setup '(slime-fancy slime-asdf))
+  )
 
 ;; ============================================================================
 ;; other modes
@@ -102,7 +103,7 @@
 
 ;; ido-mode ---------------------------
 ;; use ido-mode for switching buffers
-(ido-mode 1)
+;; (ido-mode 1)
 
 ; display choices vertically
 (setq ido-separator "\n")
