cf2eb4d7a57bb2ae910b49e3cbe5e06c4f829fdb
Khoa Le <tkhoa2711@yahoo.com.vn>
2016-03-29 13:32:35 +0800

    add tip to view buffer's full path without copying

--- .emacs.d/init/init-filesystem.el
+++ .emacs.d/init/init-filesystem.el
@@ -1,5 +1,6 @@
 ;;; init-filesystem.el
 
+;; NOTE to view the path without copying, use `M-: buffer-file-name'
 (defun copy-full-path-to-kill-ring ()
   "Copy buffer's full path to kill ring."
   (interactive)
